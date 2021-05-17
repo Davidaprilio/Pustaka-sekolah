12f214855d8e258b5983061168d6112e1ddf71f4
David Aprilio <david1.aprilio4@gmail.com>
2021-05-18 02:25:03 +0700

    add new field 'last_page_read'

--- app/Models/BookReaderModel.php
+++ app/Models/BookReaderModel.php
@@ -9,7 +9,7 @@ class BookReaderModel extends Model
 	protected $table = 'historyReading';
 	protected $useTimestamps = true;
 	protected $primaryKey = 'id';
-	protected $allowedFields = ['idUser', 'idBook', 'time', 'startTime', 'start'];
+	protected $allowedFields = ['idUser', 'idBook', 'time', 'startTime', 'start' ,'last_page_read'];
 
 	public function getUserRead(string $idBook, array $arrUser, bool $onlyCountResult = false)
 	{
